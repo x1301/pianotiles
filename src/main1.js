5e0cc670491b79ea6fda96132af37d095985c782
x1301 <92451531+x1301@users.noreply.github.com>
2021-12-08 20:22:30 +0100

    1

---
+++ src/main1.js
@@ -0,0 +1,15 @@
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540,
+    backgroundColor: '#ffffff',
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: new Tableau1()
+};
+let game = new Phaser.Game(gameConfig);
