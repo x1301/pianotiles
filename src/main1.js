273e3091146b55acc76d1ba37e2f1a4fac7d1cb5
x1301 <92451531+x1301@users.noreply.github.com>
2021-12-17 01:02:04 +0100

    update

--- src/main1.js
+++ src/main1.js
@@ -2,6 +2,18 @@ let gameConfig = {
     type: Phaser.AUTO,
     width: 978,
     height: 501,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        min: {
+            width: 978,
+            height: 501
+        },
+        max: {
+            width: 1728,
+            height: 972,
+        }
+    },
     backgroundColor: '#ffffff',
     parent: 'game',
     physics: {
